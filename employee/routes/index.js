52ae6e59d8207da3e3e133ad8a5a9ff32e9810af
Kajanan98 <Kajanan023@gmail.com>
2022-03-28 22:12:30 +0530

    route

--- employee/routes/index.js
+++ employee/routes/index.js
@@ -4,7 +4,7 @@ const { ensureEmployee } = require('../../middlewares/authMiddleware')
 
 router.use('/auth', require('./authRoute'));
 
-// router.use(ensureEmployee)
+router.use(ensureEmployee)
 router.use('/employees', require('./employeeRoute'));
 router.use('/roles', require('./roleRoute'));
 router.use('/user', require('./userRoute'));
